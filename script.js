535f6ebef3507d9aea44c4617be0bceac0737edd
Sofinaye <abene.fekadu@gmail.com>
2023-04-19 17:22:48 +0300

    Add error messege style and fix class issues

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('form');
-const alertMessage = document.querySelector('small');
+const alertMessage = document.querySelector('.error-msg');
 form.addEventListener('submit', (event) => {
   const email = document.querySelector('.email');
   const emailRegularExperession = /[A-Z]/;
