d8d5826b098430787074abb5222d4a00f06e120d
Sofinaye <abene.fekadu@gmail.com>
2023-04-19 17:08:24 +0300

    Add validation functionality using javascript validation

---
+++ script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+const alertMessage = document.querySelector('small');
+form.addEventListener('submit', (event) => {
+  const email = document.querySelector('.email');
+  const emailRegularExperession = /[A-Z]/;
+  event.preventDefault();
+  if (!emailRegularExperession.test(email.value)) {
+    form.submit();
+  } else {
+    alertMessage.innerText = '* Please Enter Your Email Only In Lower Case';
+  }
+});
